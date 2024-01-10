df4cb602a22047157b84d3f9aec3218cd6d4bf28
sjdev2212 <daniel_amoroso31@hotmail.com>
2024-01-09 23:38:33 -0300

    create updatebook

---
+++ src/components/UpdateBook.js
@@ -0,0 +1,193 @@
+import React from 'react'
+import { useState } from "react";
+import { useParams } from 'react-router-dom';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import axios from "axios";
+
+
+const UpdateBook = () => {
+
+    const id = useParams().id;
+
+
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [isbn, setIsbn] = useState("");
+    const [genre, setGenre] = useState("");
+    const [imageFile, setImageFile] = useState(null);
+    const [date, setDate] = useState("");
+
+    const handleUpdate = async (e) => {
+
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("book[title]", title);
+    formData.append("book[author]", author);
+    formData.append("book[description]", description);
+    formData.append("book[price]", price);
+    formData.append("book[isbn]", isbn);
+    formData.append("book[genre]", genre);
+    formData.append("book[date]", date);
+    formData.append("book[image]", imageFile);
+    console.log(id);
+
+
+    try {
+        const res = await axios.put(`https://rails-production-ed19.up.railway.app/api/books/${id}`, formData);
+        console.log(res);
+        } catch (err) {
+        console.log(err);
+        }
+        
+  };
+  return (
+    <div>
+  
+    <div>
+      <h2
+        style={{
+          textAlign: "center",
+          marginTop: "5vw",
+        }}
+      >
+      Update book
+      </h2>
+      <Box
+        component="main"
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Box
+          component="form"
+          onSubmit={handleUpdate}
+          noValidate
+          sx={{
+            width: "50vw",
+            height: "50vh",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            id="title"
+            label="title"
+            name="title"
+            value={title}
+            autoComplete="title"
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            id="author"
+            label="author"
+            name="author"
+            value={author}
+            autoComplete="author"
+            autoFocus
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            id="description"
+            label="description"
+            name="description"
+            type="textarea"
+            value={description}
+            autoComplete="description"
+            autoFocus
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            type="file"
+            autoFocus
+            onChange={(e) => setImageFile(e.target.files[0])}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            id="price"
+            label="price"
+            name="price"
+            value={price}
+            autoComplete="price"
+            autoFocus
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            id="date"
+            label="date"
+            name="date"
+            type="date"
+            value={date}
+            autoComplete="date"
+            autoFocus
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            id="isbn"
+            label="isbn"
+            name="isbn"
+            value={isbn}
+            autoComplete="isbn"
+            autoFocus
+            onChange={(e) => setIsbn(e.target.value)}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            id="genre"
+            label="genre"
+            name="genre"
+            value={genre}
+            autoComplete="genre"
+            autoFocus
+            onChange={(e) => setGenre(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              width: "20vw",
+              height: "50vw",
+              display: "flex",
+              flexDirection: "column",
+              margin: "auto",
+            }}
+          >
+            Add Book
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  </div>
+  )
+}
+
+export default UpdateBook
